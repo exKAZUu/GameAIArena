5f4b2720c3488afdbe8aacbb10624d9c97c7167e
Kazunori Sakamoto <exkazuu@gmail.com>
2012-05-22 22:45:07 +0900

    Add some helper methods in Scene.

--- src/main/java/jp/ac/waseda/cs/washi/gameaiarena/gui/Scene.java
+++ src/main/java/jp/ac/waseda/cs/washi/gameaiarena/gui/Scene.java
@@ -1,18 +1,13 @@
 package jp.ac.waseda.cs.washi.gameaiarena.gui;
 
+import java.util.List;
+
 import jp.ac.waseda.cs.washi.gameaiarena.common.Environment;
+import jp.ac.waseda.cs.washi.gameaiarena.key.MappedInputer;
 
 public abstract class Scene<Env extends Environment> {
 
   private Env environment;
-
-  public Env getEnvironment() {
-    return environment;
-  }
-
-  final void setEnvironment(Env environment) {
-    this.environment = environment;
-  }
 
   public void initialize() {}
 
@@ -23,4 +18,24 @@ public abstract class Scene<Env extends Environment> {
   }
 
   public void draw() {}
+
+  public final Env getEnvironment() {
+    return environment;
+  }
+
+  public final MappedInputer getInputer() {
+    return environment.getInputer();
+  }
+
+  public final Renderer getRenderer() {
+    return environment.getRenderer();
+  }
+
+  public final List<Renderer> getSubRenderers() {
+    return environment.getSubRenderers();
+  }
+
+  final void setEnvironment(Env environment) {
+    this.environment = environment;
+  }
 }
